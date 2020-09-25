a9454c7852ce3020de23fbe0d64b9cd084e3eddf
c0derbr1t <c0der.br1t@gmail.com>
2020-09-25 11:07:24 -0500

    Changed to use Routing. Add Info page.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
-// import Game from './components/Game.js';
-import Game2 from './components/Game2.js';
+// import Game2 from './components/Game2.js';
+import Game from './components/Game.js';
+import Info from './components/Info.js';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Conway's Game of Life</h1>
-      <h3>Implemented by Brittani Luce</h3>
-      <Game2 />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Conway's Game of Life</h1>
+        <h3>Implemented by Brittani Luce</h3>
+        <Route exact path="/" component={Game} />
+        <Route exact path="/info" component={Info} />
+      </div>
+    </Router>
+    
   );
 }
 
